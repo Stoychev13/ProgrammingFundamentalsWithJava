e28160c658c8c9da16cc3af1a7553d2174fc478e
Stoychev13 <61363718+Stoychev13@users.noreply.github.com>
2021-05-25 18:56:04 +0300

    BasicSyntaxConditionalStatementsAndLoopsMoreExercise

---
+++ src/com/company/BasicSyntaxConditionalStatementsAndLoopsMoreExercise/ReverseString.java
@@ -0,0 +1,13 @@
+package com.company.BasicSyntaxConditionalStatementsAndLoopsMoreExercise;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(new StringBuilder(scanner.nextLine()).reverse().toString());
+
+    }
+}
+
